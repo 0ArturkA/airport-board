6eabc851a727a3a860ae33a814db795e069a4056
Artur <exstrominer@gmail.com>
2019-03-28 08:42:09 +0300

    chore(cache): increase cache time

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cacheMiddleware(5));
+app.use(cacheMiddleware(10));
 
 // Routes
 app.use('/', indexRouter);
