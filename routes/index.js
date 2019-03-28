f55d1daed2bf6a37104554045e7409cb1b02df9f
Artur <exstrominer@gmail.com>
2019-03-28 08:41:50 +0300

    feat(pagination): add changing offset

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,32 @@
 const { Router } = require('express');
+const createError = require('http-errors');
 const router = Router();
 
 const { getSchedule } = require('../services/yandex-schedule');
+
+const IATA_STATION = 'SIP';
+const ITEMS_PER_PAGE = 10;
 
 router.get('/', (_req, res) => res.redirect('/timetable/departure'));
 
 router.get('/timetable/:event', (req, res, next) => {
   const { event } = req.params;
+  const { offset = 0 } = req.query;
 
-  getSchedule(
-    'SIP',
-    new Date().toLocaleDateString(),
-    'plane',
-    'iata',
-    'Europe/Moscow',
-    event
-  )
+  if (event !== 'departure' && event !== 'arrival') {
+    return next(createError.BadRequest('Invalid event'));
+  }
+
+  getSchedule({
+    station: IATA_STATION,
+    date: new Date().toLocaleDateString(),
+    transport_types: 'plane',
+    system: 'iata',
+    result_timezone: 'Europe/Moscow',
+    event,
+    limit: ITEMS_PER_PAGE,
+    offset
+  })
     .then(data => res.render('index', data))
     .catch(next);
 });
--- services/yandex-schedule.js
+++ services/yandex-schedule.js
@@ -3,24 +3,10 @@ const qs = require('querystring');
 
 const mapSchedule = require('../mappings/timetable');
 
-module.exports.getSchedule = (
-  station,
-  date,
-  transportTypes,
-  system,
-  result_timezone,
-  event
-) => {
-  const params = qs.stringify({
-    station,
-    date,
-    transport_types: transportTypes,
-    system,
-    result_timezone,
-    event
-  });
+module.exports.getSchedule = params => {
+  const query = qs.stringify(params);
 
-  return fetch(`https://api.rasp.yandex.net/v3.0/schedule/?${params}`, {
+  return fetch(`https://api.rasp.yandex.net/v3.0/schedule/?${query}`, {
     headers: {
       Authorization: process.env.YANDEX_SCHEDULE_API
     }
